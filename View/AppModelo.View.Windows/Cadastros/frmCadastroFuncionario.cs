6a8cc7f556b86dcd3f6ec159e1d27bdf49f6443a
Shamyra <shamyramessias2811@gmail.com>
2022-10-14 19:16:54 -0300

    Atualização

---
+++ View/AppModelo.View.Windows/Cadastros/frmCadastroFuncionario.cs
@@ -0,0 +1,29 @@
+﻿using AppModelo.Controller.External;
+using AppModelo.View.Windows.Helpers;
+using System.Windows.Forms;
+
+namespace AppModelo.View.Windows.Cadastros
+{
+    public partial class frmCadastroFuncionario : Form
+    {
+        public frmCadastroFuncionario()
+        {
+            InitializeComponent();
+            Componentes.FormatarCamposObrigatorios(this);
+        }
+
+        private void btnPesquisarCep_Click(object sender, System.EventArgs e)
+        {
+            //Crio a instancia do Controllador
+            var cepController = new ViaCepController();
+
+            //Recebo os dados do metodo obter para o endereço
+            var endereco = cepController.Obter(txtEnderecoCep.Text);
+
+            txtEnderecoBairro.Text = endereco.Bairro;
+            txtEnderecoLogradouro.Text = endereco.Logradouro;
+            txtEnderecoMunicipio.Text = endereco.Localidade;
+            txtEnderecoUf.Text = endereco.Uf;
+        }
+    }
+}
